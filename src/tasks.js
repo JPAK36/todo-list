710d90f52fdefd2ad524c42a0d9c18c6699f2b10
JP Khoury <JPKhoury96@gmail.com>
2021-12-18 16:05:44 -0800

    Add functionality to edit task button

--- src/tasks.js
+++ src/tasks.js
@@ -19,7 +19,24 @@ const addTaskToDOM = (addTaskBtn) => {
     const iconSpan = createElements().createSpanElement('icons');
     const editIcon = createElements().createImageElement('images/edit-icon.svg', 'edit-icon');
     const deleteIcon = createElements().createImageElement('images/delete-icon.svg', 'delete-icon');
-    
+
+    editIcon.addEventListener('click', () => {
+        if (taskListElement.classList.contains('completed')) return;
+        const taskTextSpan = document.querySelector('.task-text');
+        const task = taskTextSpan.textContent;
+        taskTextSpan.remove();
+        
+        const inputForm = createElements().createForm();
+        const inputField = createElements().createInputField('Enter Task...');
+
+        inputField.value = task;
+        
+        inputForm.append(inputField);
+        taskSpan.insertBefore(inputForm, iconSpan);
+        taskListElement.prepend(taskSpan);
+        addTask();
+    });
+
     deleteIcon.addEventListener('click', () => {
         taskListElement.remove();
         // TODO: Remove task from localStorage
@@ -91,13 +108,16 @@ const validateUserInput = () => {
     const iconSpan = taskItem.lastElementChild;
 
     const userInput = inputField.value.trim();
-    
+    const taskTextSpan = createElements().createSpanElement('task-text');
+    taskTextSpan.append(userInput);
+
         if (userInput == '') {
             taskContainer.remove();
             return;
         }
         inputForm.remove();
-        iconSpan.insertAdjacentText('beforebegin', userInput);
+        taskItem.prepend(taskTextSpan);
+        //iconSpan.insertAdjacentText('beforebegin', userInput);
 }
 
 // TODO: Update priority in localStorage
